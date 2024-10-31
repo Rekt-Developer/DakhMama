a5ba05105cd8659375a5ccf1410036563c31bc14
Crimson-streaming <aamarrelam@icloud.com>
2024-10-31 15:22:39 +0100

    sw update

--- sw/service-worker.js
+++ sw/service-worker.js
@@ -1,42 +1,75 @@
-// service-worker.js
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.3/workbox-sw.js');
 
-// Activer le précaching des fichiers
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST || []);
+// Nom du cache pour les ressources précachées
+const PRECACHE = 'precache-v2';
 
-// Mise en cache des fichiers CSS et JS
-workbox.routing.registerRoute(
-    ({ request }) => request.destination === 'style' || request.destination === 'script',
-    new workbox.strategies.CacheFirst({
-        cacheName: 'static-resources',
-        plugins: [
-            {
-                cacheWillUpdate: async ({ request, response }) => {
-                    if (!response || response.status !== 200) {
-                        return null;
-                    }
-                    return response;
-                },
-            },
-        ],
-    })
-);
+// Liste des ressources locales à précharger
+const PRECACHE_URLS = [
+    '../hors-ligne.html', 
+    '../css/bootstrap.min.css',
+    '../css/font-awesome.min.css',
+    '../css/ionicons.css',
+    '../css/jquery-eu-cookie-law-popup.css',
+    '../css/megamenu.css',
+    '../css/owl.carousel.min.css',
+    '../css/player-crimson.css',
+    '../css/responsive.css',
+    '../css/search.css',
+    '../css/splide.min.css',
+    '../css/style-three.css',
+    '../css/validatesecurity.css',
+    '../js/bootstrap.min.js',
+    '../js/custom-main.js',
+    '../js/FWDEVPlayer.js',
+    '../js/jquery-3.3.1.min.js',
+    '../js/jquery.easing.min.js',
+    '../js/jquery.nice-select.min.js',
+    '../js/megamenu.js',
+    '../js/owl.carousel.min.js',
+    '../js/splide.min.js',
+    '../js/sweetalert2@11.js',
+    '../img/logo.png',
+    '../img/hors-ligne.png',
+    '../img/logo-mobile.png'
+];
 
-// Mise en cache des images
-workbox.routing.registerRoute(
-    ({ request }) => request.destination === 'image',
-    new workbox.strategies.CacheFirst({
-        cacheName: 'images',
-        plugins: [
-            {
-                cacheWillUpdate: async ({ request, response }) => {
-                    if (!response || response.status !== 200) {
-                        return null;
-                    }
-                    return response;
-                },
-            },
-        ],
-    })
-);
+// Gestionnaire d'installation : précache les ressources nécessaires
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
+});
+
+// Gestionnaire d'activation : nettoyage des anciens caches
+self.addEventListener('activate', event => {
+  const currentCaches = [PRECACHE];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+        return caches.delete(cacheToDelete);
+      }));
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Gestionnaire de récupération avec redirection vers hors-ligne.html si offline
+self.addEventListener('fetch', event => {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        // Si la ressource est en cache, on la renvoie
+        if (response) {
+          return response;
+        }
+        // Si la ressource n'est pas en cache, on essaie de la récupérer
+        return fetch(event.request).catch(() => {
+          // Si la requête échoue (offline), on renvoie hors-ligne.html
+          return caches.match('../hors-ligne.html');
+        });
+      })
+    );
+  });
+  
