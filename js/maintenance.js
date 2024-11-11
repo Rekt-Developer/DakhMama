c764ff750dd60c55f9206246645829f8d3931c75
Crimson-streaming <aamarrelam@icloud.com>
2024-11-11 18:17:19 +0100

    update

--- js/maintenance.js
+++ js/maintenance.js
@@ -1,3 +1,4 @@
+// Redirige vers la page de maintenance si l'URL n'est pas déjà celle de maintenance
 if (!window.location.pathname.includes("index.html")) {
-    window.location.href = "/index.html";
+    window.location.href = "https://crimson-streaming.github.io/crimson";
 }
