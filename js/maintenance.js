574658b985be3608da41057dc5e87a6affdeb5d3
Crimson-streaming <aamarrelam@icloud.com>
2024-11-11 18:24:15 +0100

    update

--- js/maintenance.js
+++
@@ -1,4 +0,0 @@
-// Redirige vers la page de maintenance si l'URL n'est pas déjà celle de maintenance
-if (!window.location.pathname.includes("../index.html")) {
-    window.location.href = "../index.html"; // Utilise le chemin relatif pour éviter une redirection absolue
-}
